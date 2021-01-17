04926091d36a13e2324c8aa3023b7dbeca894723
tbrms <tbrms@tbrms.com>
2021-01-17 15:27:15 +0100

    Ajout du script sql de debug

--- meteo.sql
+++ meteo.sql
@@ -1,5 +1,20 @@
+drop table bulletin_meteo;
+
+
 truncate bulletin_meteo ;
+
+ALTER SEQUENCE hibernate_sequence RESTART WITH 1;
+
 
 select * from bulletin_meteo bm ;
 
-ALTER SEQUENCE hibernate_sequence RESTART WITH 1;
+select temperature from bulletin_meteo where id=46;
+
+CREATE TABLE bulletin_meteo (
+    id   int8      NOT NULL,
+    date timestamp NOT NULL,
+    humidite int4 NOT NULL,
+    pression int4 NOT NULL,
+    temperature numeric(19,1) NOT NULL,
+    PRIMARY KEY (id)
+);
