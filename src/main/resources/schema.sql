a190bdce31f58e26b5099af03becd7fc77e272a2
tbrms <tbrms@tbrms.com>
2021-01-17 15:28:44 +0100

    Ajout du script sql de debug

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE bulletin_meteo (
+    id   int8      NOT NULL,
+    date timestamp NOT NULL,
+    humidite int4 NOT NULL,
+    pression int4 NOT NULL,
+    temperature numeric(19,1) NOT NULL,
+    PRIMARY KEY (id)
+);
