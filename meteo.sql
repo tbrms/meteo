64414d8e3214cc6215a2a1198c915a8e5a1be902
tbrms <tbrms@tbrms.com>
2021-03-22 15:24:45 +0100

    MAJ du fichier de requetes SQL

--- meteo.sql
+++ meteo.sql
@@ -3,12 +3,16 @@ drop table bulletin_meteo;
 
 truncate bulletin_meteo ;
 
-ALTER SEQUENCE hibernate_sequence RESTART WITH 1;
+ALTER SEQUENCE hibernate_sequence RESTART WITH 1316;
 
 
 select * from bulletin_meteo bm ;
 
 select temperature from bulletin_meteo where id=46;
+
+select * from bulletin_meteo bm order by id desc ;
+
+delete from bulletin_meteo where id=1317;
 
 CREATE TABLE bulletin_meteo (
     id   int8      NOT NULL,
