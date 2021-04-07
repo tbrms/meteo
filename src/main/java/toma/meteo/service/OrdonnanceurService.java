6458a78ebc1361b5ee1ac8236764ad198a54c924
tbrms <mail.dustmail@gmail.com>
2021-04-07 10:04:46 +0200

    Ajout d un logger dans OrdonnanceurService

--- src/main/java/toma/meteo/service/OrdonnanceurService.java
+++ src/main/java/toma/meteo/service/OrdonnanceurService.java
@@ -1,6 +1,9 @@
 package toma.meteo.service;
 
 import toma.meteo.bean.BulletinMeteo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,8 @@ public class OrdonnanceurService {
 	private static final String PORT="8081";
 	private static final String CONTEXT="meteo";
 	private static final String REQUEST="getReleveMeteo";
+	
+	private static Logger logger = LogManager.getLogger(OrdonnanceurService.class);
 
 	@Autowired
 	BulletinMeteoService bulletinMeteoService;
@@ -29,7 +34,7 @@ public class OrdonnanceurService {
 	@Scheduled(cron = "${cron.expression}")
 	public void insertBulletinMeteo() {
 		Optional<BulletinMeteo> bulletinMeteo = arduinoService.getBulletinMeteo();
-
+		logger.debug("Recuperation d'un releve depuis Arduino: " + bulletinMeteo.toString());
 		if(bulletinMeteo.isPresent()){
 			bulletinMeteoService.ajouter(bulletinMeteo.get());
 		}
