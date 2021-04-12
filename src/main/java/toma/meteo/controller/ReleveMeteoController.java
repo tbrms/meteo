f4959da57facc1f9d5be97865a093e5bcf077ec2
tbrms <mail.dustmail@gmail.com>
2021-04-12 08:36:01 +0200

    Activation du CrossOrigin

--- src/main/java/toma/meteo/controller/ReleveMeteoController.java
+++ src/main/java/toma/meteo/controller/ReleveMeteoController.java
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,7 +74,7 @@ public class ReleveMeteoController {
 	/*
 	 * Recuperer un bulletin meteo en direct
 	 */
-	//@CrossOrigin(origins = "*")
+	@CrossOrigin(origins = "*")
 	@GetMapping("/getBulletinMeteoNow")
 	public BulletinMeteoExt getReleveMeteoNow() {
 		Optional<BulletinMeteoExt> bulletin = arduinoService.getBulletinMeteo();
